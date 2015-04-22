2ef64377d235143af20c9d50a2a37bf3121be07d
kimbvn <kimbvn@bc.edu>
2015-04-22 16:41:29 -0400

    Create dbconn.php

---
+++ dbconn.php
@@ -0,0 +1,18 @@
+<?php
+function connectToDB() {
+	$dbc = @mysqli_connect("localhost","kimbvn","agTKw48F","kimbvn") or
+		die("Connect failed:".mysqli_connect_error());
+	return ($dbc);
+}
+
+function disconnectFromDB($dbc,$result) {
+	mysqli_free_result($result);
+	mysqli_close($dbc);
+}
+
+function performQuery($dbc,$query) {
+	$result = mysqli_query($dbc,$query) or die("bad query".mysqli_error($dbc));
+	return ($result);
+}
+
+?>
