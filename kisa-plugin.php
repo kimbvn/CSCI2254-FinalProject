a58bf3d1a4762df708e84acd28a5ecd74e19e2e3
kimbvn <kimbvn@bc.edu>
2015-05-06 14:40:52 -0400

    Update kisa-plugin.php

--- kisa-plugin.php
+++ kisa-plugin.php
@@ -50,4 +50,20 @@ function register_cpt_album() {
 
     register_post_type( 'album', $args );
 }
+ function display_child() {
+ 	global $post;
+ 		if( is_page() && $post->post_parent) {
+ 			$childpages = wp_list_pages('sort_colun=menu_order&title_li=&child_of=' .$post-
+ 			>post_parent .'&echo=0');
+ 		} else {
+ 			$childpages = wp_list_pages('sort_column=menu_order&title_li=&child_of=' .$post->ID .
+ 			'&echo=0');
+ 		}
+ 		if($childpages) {
+ 	 		$string = '<ul>'.$childpages.'</ul'>;
+ 	 	}
+ 	 	return $string;
+ 	}
+ 	add_shortcode('wpb_childpages','wpb_list_child_pages');
+
 
