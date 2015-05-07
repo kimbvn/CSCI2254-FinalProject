7a02c471b41ba86d4f6969d3f8cc70ff7a20c1aa
kimbvn <kimbvn@bc.edu>
2015-05-06 20:24:36 -0400

    Update kisa-plugin.php

--- kisa-plugin.php
+++ kisa-plugin.php
@@ -6,7 +6,9 @@ Description: Site specific code changes for example.com
 
 */
 
+
 add_image_size( 'album-grid', 225, 150, true );
+
 
 add_action( 'init', 'register_cpt_album' );
 
@@ -50,21 +52,88 @@ function register_cpt_album() {
 
     register_post_type( 'album', $args );
 }
+
+function be_attachment_field_credit( $form_fields, $post ) {
+
+    $form_fields['hashtag'] = array(
+
+        'label' => 'hashtag#',
+        'input' => 'text',
+        'value' => get_post_meta( $post->ID, 'hashtag', true ),
+        'helps' => 'Add #hashtags!',
+    );
+ 
+ 
+    return $form_fields;
+
+}
+
+
+add_filter( 'attachment_fields_to_edit', 'be_attachment_field_credit', 10, 2 );
+
+/**
+
+ * Save values of hashtags in media uploader
+
+ * @param $post array, the post data for database
+
+ * @param $attachment array, attachment fields from $_POST form
+
+ * @return $post array, modified post data
+
+ */
+
+function be_attachment_field_credit_save( $post, $attachment ) {
+
+    if( isset( $attachment['hashtag'] ) )
+        update_post_meta( $post['ID'], 'hashtag', $attachment['hashtag'] );
+ 
+    return $post;
+}
+ 
+
+add_filter( 'attachment_fields_to_save', 'be_attachment_field_credit_save', 10, 2 );
+
+
+/*
+ * Displays a list of child pages under a paent page, KISA YOUTUBE
+ */
+ 
  function display_child() {
  	global $post;
  		if( is_page() && $post->post_parent) {
- 			$childpages = wp_list_pages('sort_colun=menu_order&title_li=&child_of=' .$post-
- 			>post_parent .'&echo=0');
+ 			$childpages = wp_list_pages('sort_colun=menu_order&title_li=&child_of=' .$post->
+ 			post_parent .'&echo=0');
  		} else {
  			$childpages = wp_list_pages('sort_column=menu_order&title_li=&child_of=' .$post->ID .
  			'&echo=0');
  		}
  		if($childpages) {
- 	 		$string = '<ul>'.$childpages.'</ul'>;
+ 	 		 $string = '<ul>' . $childpages . '</ul>';
+
  	 	}
- 	 	return $string;
+ 	 	echo $string;
  	}
- 	add_shortcode('wpb_childpages','wpb_list_child_pages');
+ 	add_shortcode('wpb_childpages','display_child');
+ 	add_filter('widget_text','do_shortcode');
+/*
+function getweather(){
+	$weatherlocs = "http://w1.weather.gov/xml/current_obs/KBOS.xml"
+	$xml = new SimpleXMLElement(file_get_contents("boston"));
+	$location = $xml->location;
+	$temp = $xml->temp_f;
+	$visibility = $xml->visibility_mi;
+	$time = $xml->observation_time;
+	echo "<h2>The weather of $location</h2>";
+	echo "Temperature is $temp'F and the visibility is $visibility<br>";
+	echo "$time";
+
+}
+
+
+add_shortcode('weather','getweather');
+*/
+
 
 function displaysoccer() {
 	echo "<div style=\"float:bottom; text-align:center\">";
@@ -112,25 +181,28 @@ function displaysoccer() {
 			</tr>
 		</table></div>";		
 	echo "</form>";
+}
+add_shortcode('soccerform','displaysoccer');
+
+
+
+if(isset($_POST['submit'])) {
+	handlesoccer();
 	}
-	add_shortcode('soccerform','displaysoccer');
-
-
-
-
 
 
 function handlesoccer() {
 
 	
-	$name = $_POST['players'];
-	$star = $_POST['school'];
-	$price = $_POST['number2'];
+	$plyers = $_POST['players'];
+	$school = $_POST['school'];
+	$players2 = $_POST['number2'];
 	$address = $_POST['address'];
+	$date = $_POST['date'];
 	$phone = $_POST['phone'];
 	$comment = mysql_real_escape_string($_POST['comment']);
 	
-
+	echo "\$players, \$school,\$players2\\";
    	$geocodeURL = "https://maps.googleapis.com/maps/api/geocode/xml?";
    	$urladdress = "address=" . urlencode($address);
    	$key = "AIzaSyC0l5lpV9qWcYUuxB2jbSw3gMuyumBfs5g";
@@ -139,7 +211,6 @@ function handlesoccer() {
    	$xml= new SimpleXMLElement( file_get_contents( $geocoderequest ) );
    		
    	if($xml->status != 'OK') {
-   		header("Location: index.php?status=$xml->error_message");
    		$status = $xml->error_message;
    		die("bad result status $status");
    	}
@@ -147,13 +218,43 @@ function handlesoccer() {
     $latitude  = $xml->result->geometry->location->lat;
     $longitude = $xml->result->geometry->location->lng;
         
-    if(insertform($name,$star,$price,$category,$address,$phone,$url,$userid,$comment,$latitude,$longitude)) {
-    	header('Location: index.php?status=goodinsert');
+    if(insertform($players,$school,$players2,$address,$date,$phone,$comment)) {
+		echo "Insertion done";
     } else {
-    	header('LocationL index.php?status=badaddr');
+    	echo "Insertion failed";
     }
-
+    
+    
 }
-add_shortcode('soccermap','handlesoccer');
+//add_shortcode('soccermap','handlesoccer');
+
+function connectToDB() {
+	$dbc = @mysqli_connect("localhost","kimbvn","agTKw48F","kimbvn") or
+		die("Connect failed:".mysqli_connect_error());
+	return ($dbc);
+}
+
+function disconnectFromDB($dbc,$result) {
+	mysqli_free_result($result);
+	mysqli_close($dbc);
+}
+
+function performQuery($dbc,$query) {
+	$result = mysqli_query($dbc,$query) or die("bad query".mysqli_error($dbc));
+	return ($result);
+}
+
+function insertform($players,$school,$players2,$address,$date,$phone,$comment) {
+	$dbc = connectToDB();
+	$query = "INSERT INTO KISASoccer VALUES(\"$players\",\"$school\",\"$players2\",
+				\"$address\",\"$date\",\"$phone\",\"$comment\")";
+	$result = performQuery($dbc,$query);
+	if($result == 1) {
+		return true;
+		
+	} else {
+		return false;
+	}
+}
 
 ?>
